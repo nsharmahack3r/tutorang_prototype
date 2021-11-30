9de4375ae66bb38af3ee156c0ced241553b3465e
“Nabhya <svirajhack3r@gmail.com>
2021-12-01 01:09:21 +0530

    Checkout page added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,3 +14,12 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+function showNavbarItems(){
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
